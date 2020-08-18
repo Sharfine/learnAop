961ae2fa8f5a7617e7ff43a8618f423488b0743f
sharfine <784751530@qq.com>
2020-08-18 20:05:42 +0800

    :sparkles: add mongoTest

---
+++ src/main/java/com/sharfine/validate/testcode/list/LinkedListTest.java
@@ -0,0 +1,14 @@
+package com.sharfine.validate.testcode.list;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author: Sharfine
+ * @createTime: 2020/8/18 19:55
+ */
+public class LinkedListTest {
+    public static void main(String[] args) {
+        List list = new LinkedList();
+    }
+}
---
+++ src/main/java/com/sharfine/validate/testcode/test/model/Dog.java
@@ -0,0 +1,13 @@
+package com.sharfine.validate.testcode.test.model;
+
+import lombok.Data;
+
+/**
+ * @author: Sharfine
+ * @createTime: 2020/8/17 11:56
+ */
+@Data
+public class Dog {
+    private String name;
+
+}
